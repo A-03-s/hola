d14367113ef0d9840144884521b2239964d69232
Ash <lab.a03s.ar@gmail.com>
2023-01-19 12:46:40 -0300

    Primer Commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import '../style.css'
+const gifts = ['book', 'game', 'socks']
+const wrapped = wrapping(gifts)
+
+console.log(wrapped)
+
+function wrapping(sGift) {
+    const arr_Gift = []
+    l0 = sGift.length	
+    for( j=0; j<l0; j++){
+        myGift = sGift[j]
+        myGift = "\n*" + myGift + "*\n"
+        l = myGift.length - 2
+        for( i=0; i<l; i++){
+            myGift="*"+myGift+"*"
+            }
+        arr_Gift[j] = myGift;
+        }		
+    return arr_Gift;
+    }
